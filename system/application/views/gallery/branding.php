1b3d4f176bf43e1583d0fb2cd8df97d7883633b8
mat sadler <mat@redstudio.co.uk>
2011-01-14 16:12:31 +0000

    updated galleries

--- system/application/views/gallery/branding.php
+++ system/application/views/gallery/branding.php
@@ -1,39 +1,39 @@
 <?php $location = "branding"; ?>
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/01_Autosafe_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image gallery_left" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/01_Autosafe_Thumb.jpg"></a>
 
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/02_Cathesis_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image gallery_left" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/02_Cathesis_Thumb.jpg"></a>
 
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/03_Fluid_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image right" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/03_Fluid_Thumb.jpg"></a>
 
 
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/04_Geenii_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image gallery_left" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/04_Geenii_Thumb.jpg"></a>
 
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/05_Hazledine_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image gallery_left" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/05_Hazeldine_Thumb.jpg"></a>
 
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/06_Holiday_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image right" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/06_Holiday_Thumb.jpg"></a>
 
 
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/07_County_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image gallery_left" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/07_County_Thumb.jpg"></a>
 
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/08_NWM_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image gallery_left" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/08_NWM_Thumb.jpg"></a>
 
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/09_Stonebridge_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image right" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/09_Stonebridge_Thumb.jpg"></a>
 
 
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/10_Strings_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image gallery_left" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/10_Strings_Thumb.jpg"></a>
 
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/11_PBCS_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image gallery_left" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/11_PBCS_Thumb.jpg"></a>
 
-<a  href="<?=base_url()?>images/ourwork/branding.jpg" rel="prettyPhoto[pp_gal]">
+<a  href="<?=base_url()?>images/ourwork/<?=$location?>/12_Accuro_ENLARGE.jpg" rel="prettyPhoto[pp_gal]">
 <img align="left" class="gallery_image right" src="<?=base_url()?>images/ourwork/<?=$location?>/thumbs/12_Accuro_Thumb.jpg"></a>
